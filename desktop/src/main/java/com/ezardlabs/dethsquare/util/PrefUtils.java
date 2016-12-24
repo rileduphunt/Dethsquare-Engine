ad6189ae288bb205d6b44f223926a4ce74110d83
Ezard <ezard.ben@gmail.com>
2016-12-24 03:41:00 +0000

    Move prefs code into its own class

---
+++ desktop/src/main/java/com/ezardlabs/dethsquare/util/PrefUtils.java
@@ -0,0 +1,41 @@
+package com.ezardlabs.dethsquare.util;
+
+import java.nio.charset.Charset;
+import java.util.prefs.Preferences;
+
+public class PrefUtils {
+	private static final Preferences prefs = Preferences.userRoot().node("com/ezardlabs/lostsector");
+
+	public static void setBoolean(String key, boolean value) {
+		prefs.putBoolean(key, value);
+	}
+
+	public static void setInt(String key, int value) {
+		prefs.putInt(key, value);
+	}
+
+	public static void setFloat(String key, float value) {
+		prefs.putFloat(key, value);
+	}
+
+	public static void setString(String key, String value) {
+		prefs.putByteArray(key, value.getBytes(Charset.forName("UTF-8")));
+	}
+
+	public static boolean getBoolean(String key, boolean defaultValue) {
+		return prefs.getBoolean(key, defaultValue);
+	}
+
+	public static int getInt(String key, int defaultValue) {
+		return prefs.getInt(key, defaultValue);
+	}
+
+	public static float getFloat(String key, float defaultValue) {
+		return prefs.getFloat(key, defaultValue);
+	}
+
+	public static String getString(String key, String defaultValue) {
+		return new String(prefs.getByteArray(key, defaultValue.getBytes(Charset.forName("UTF-8"))), Charset.forName("UTF-8"));
+	}
+
+}
