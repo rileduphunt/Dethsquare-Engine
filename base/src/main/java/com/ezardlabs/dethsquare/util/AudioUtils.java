9fdfc1340149dbed11a986067b3d4dfc86832485
Ezard <ezard.ben@gmail.com>
2016-12-26 15:40:07 +0000

    Add documentation

--- base/src/main/java/com/ezardlabs/dethsquare/util/AudioUtils.java
+++ base/src/main/java/com/ezardlabs/dethsquare/util/AudioUtils.java
@@ -1,20 +1,64 @@
 package com.ezardlabs.dethsquare.util;
 
+/**
+ * Interface for managing audio in a platform-specific way
+ */
 public interface AudioUtils {
-
+	/**
+	 * Create and initialise the audio from the given path
+	 *
+	 * @param id   The ID of the audio clip
+	 * @param path The path to the audio file
+	 */
 	void create(int id, String path);
 
+	/**
+	 * Play the audio file associated with the given audio clip ID
+	 *
+	 * @param id The ID of the audio clip
+	 */
 	void play(int id);
 
+	/**
+	 * Pause the audio file associated with the given audio clip ID
+	 *
+	 * @param id The ID of the audio clip
+	 */
 	void pause(int id);
 
+	/**
+	 * Stop the audio file associated with the given audio clip ID
+	 *
+	 * @param id The ID of the audio clip
+	 */
 	void stop(int id);
 
+	/**
+	 * Specify whether the audio clip with the given ID should loop or not
+	 *
+	 * @param id   The ID of the audio clip
+	 * @param loop Whether to loop or not
+	 */
 	void setLoop(int id, boolean loop);
 
+	/**
+	 * Specify the volume of the audio clip with the given ID
+	 *
+	 * @param id     The ID of the audio clip
+	 * @param volume The volume to set
+	 */
 	void setVolume(int id, int volume);
 
+	/**
+	 * Destroy the audio file associated with the given audio clip ID
+	 *
+	 * @param id The ID of the audio clip
+	 */
 	void destroy(int id);
 
+	/**
+	 * Free all memory currently being used for managing, storing and
+	 * playing audio
+	 */
 	void destroyAll();
 }
--- base/src/main/java/com/ezardlabs/dethsquare/util/PrefUtils.java
+++ base/src/main/java/com/ezardlabs/dethsquare/util/PrefUtils.java
@@ -1,19 +1,78 @@
 package com.ezardlabs.dethsquare.util;
 
+/**
+ * Interface for managing preferences in a platform-specific way
+ */
 public interface PrefUtils {
+	/**
+	 * Set a boolean value
+	 *
+	 * @param key   The name of the preference to modify
+	 * @param value The new value for the preference
+	 */
 	void setBoolean(String key, boolean value);
 
+	/**
+	 * Set an int value
+	 *
+	 * @param key   The name of the preference to modify
+	 * @param value The new value for the preference
+	 */
 	void setInt(String key, int value);
 
+	/**
+	 * Set a float value
+	 *
+	 * @param key   The name of the preference to modify
+	 * @param value The new value for the preference
+	 */
 	void setFloat(String key, float value);
 
+	/**
+	 * Set a string value
+	 *
+	 * @param key   The name of the preference to modify
+	 * @param value The new value for the preference
+	 */
 	void setString(String key, String value);
 
+	/**
+	 * Retrieve a boolean value
+	 *
+	 * @param key          The name of the preference to retrieve
+	 * @param defaultValue Value to return if this preference does not exist
+	 * @return Returns the preference value if it exists, or defaultValue if
+	 * it does not
+	 */
 	boolean getBoolean(String key, boolean defaultValue);
 
+	/**
+	 * Retrieve a boolean value
+	 *
+	 * @param key          The name of the preference to retrieve
+	 * @param defaultValue Value to return if this preference does not exist
+	 * @return Returns the preference value if it exists, or defaultValue if
+	 * it does not
+	 */
 	int getInt(String key, int defaultValue);
 
+	/**
+	 * Retrieve a boolean value
+	 *
+	 * @param key          The name of the preference to retrieve
+	 * @param defaultValue Value to return if this preference does not exist
+	 * @return Returns the preference value if it exists, or defaultValue if
+	 * it does not
+	 */
 	float getFloat(String key, float defaultValue);
 
+	/**
+	 * Retrieve a boolean value
+	 *
+	 * @param key          The name of the preference to retrieve
+	 * @param defaultValue Value to return if this preference does not exist
+	 * @return Returns the preference value if it exists, or defaultValue if
+	 * it does not
+	 */
 	String getString(String key, String defaultValue);
 }
