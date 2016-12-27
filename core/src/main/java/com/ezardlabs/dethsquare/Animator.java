2e9a33e6c3f5ecbec0d817d1974a0814cb43a627
Ezard <ezard.ben@gmail.com>
2016-12-27 18:10:06 +0000

    Minor fix

--- core/src/main/java/com/ezardlabs/dethsquare/Animator.java
+++ core/src/main/java/com/ezardlabs/dethsquare/Animator.java
@@ -42,6 +42,7 @@ public final class Animator extends Script implements Iterable<Animation> {
 					animations[index].listener.onAnimationFinished(this);
 					finished = true;
 				}
+				frame = tempFrame;
 				return;
 			} else {
 				finished = false;
@@ -65,6 +66,7 @@ public final class Animator extends Script implements Iterable<Animation> {
 			if (i != index && animations[i].name.equals(animationName)) {
 				index = i;
 				frame = 0;
+				finished = false;
 				nextFrameTime = System.currentTimeMillis() + animations[index].frameDuration;
 				gameObject.renderer.sprite = animations[index].frames[frame];
 				if (animations[index].listener != null) animations[index].listener.onAnimatedStarted(this);
