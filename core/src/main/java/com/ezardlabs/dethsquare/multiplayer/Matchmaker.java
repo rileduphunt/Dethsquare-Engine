79a8397956f9374fcaa61bf2a5eb9e556d500f5f
Ezard <ezard.ben@gmail.com>
2017-01-15 03:44:23 +0000

    Move matchmaking netcode into custom inner class

--- core/src/main/java/com/ezardlabs/dethsquare/multiplayer/Matchmaker.java
+++ core/src/main/java/com/ezardlabs/dethsquare/multiplayer/Matchmaker.java
@@ -10,64 +10,47 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.net.SocketException;
 
 public class Matchmaker implements NetworkConstants {
-	private final InetSocketAddress server;
+	private final MatchmakingThread matchmakingThread;
 
 	public Matchmaker(String serverIp, int serverPort) {
-		this.server = new InetSocketAddress(serverIp, serverPort);
+		matchmakingThread = new MatchmakingThread(new InetSocketAddress(serverIp, serverPort));
+		matchmakingThread.start();
 	}
 
 	public void findGame(MatchmakingListener listener) {
-		CompletableFuture<String> task = CompletableFuture.supplyAsync(() -> {
-			byte[] bytes = MATCHMAKING_JOIN.getBytes();
-			try (DatagramSocket socket = new DatagramSocket(Network.myPort)) {
-				socket.send(new DatagramPacket(bytes, bytes.length, server));
-				DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-				socket.receive(packet);
-				return new String(packet.getData());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return null;
-		});
+		matchmakingThread.send(MATCHMAKING_JOIN);
 		GameListeners.addUpdateListener(new UpdateListener() {
 			@Override
 			public void onUpdate() {
-				if (task.isDone()) {
+				if (matchmakingThread.isDone()) {
 					GameListeners.removeUpdateListener(this);
-					try {
-						String data = task.get();
-						if (data == null) {
-							listener.onError("Server error");
-						} else {
-							JSONObject json = new JSONObject(data);
-							String message = json.getString("message");
-							switch (message) {
-								case GAME_CREATE:
-									listener.onCreateGame();
-									Matchmaker.this.registerNewGame();
-									break;
-								case GAME_JOIN:
-									JSONArray jsonPlayers = json.getJSONArray("players");
-									NetworkPlayer[] players = new NetworkPlayer[jsonPlayers
-											.length()];
-									for (int i = 0; i < players.length; i++) {
-										players[i] = NetworkPlayer
-												.fromJson(jsonPlayers.getJSONObject(i));
-									}
-									listener.onGameFound(json.getInt("playerId"), players);
-									break;
-								default:
-									listener.onError("Unknown error");
-									break;
-							}
+					String data = matchmakingThread.get();
+					if (data == null) {
+						listener.onError("Server error");
+					} else {
+						JSONObject json = new JSONObject(data);
+						String message = json.getString("message");
+						switch (message) {
+							case GAME_CREATE:
+								listener.onCreateGame();
+								Matchmaker.this.registerNewGame();
+								break;
+							case GAME_JOIN:
+								JSONArray jsonPlayers = json.getJSONArray("players");
+								NetworkPlayer[] players = new NetworkPlayer[jsonPlayers.length()];
+								for (int i = 0; i < players.length; i++) {
+									players[i] = NetworkPlayer
+											.fromJson(jsonPlayers.getJSONObject(i));
+								}
+								listener.onGameFound(json.getInt("playerId"), players);
+								break;
+							default:
+								listener.onError("Unknown error");
+								break;
 						}
-					} catch (InterruptedException | ExecutionException e) {
-						e.printStackTrace();
-						listener.onError("Internal error");
 					}
 				}
 			}
@@ -75,14 +58,55 @@ public class Matchmaker implements NetworkConstants {
 	}
 
 	private void registerNewGame() {
-		new Thread(() -> {
-			byte[] bytes = GAME_CREATE.getBytes();
-			try (DatagramSocket socket = new DatagramSocket(Network.myPort)) {
-				socket.send(new DatagramPacket(bytes, bytes.length, server));
-			} catch (IOException e) {
+		matchmakingThread.send(GAME_CREATE);
+	}
+
+	private class MatchmakingThread extends Thread {
+		private final InetSocketAddress server;
+		private final DatagramSocket socket;
+		private volatile String message;
+		private volatile String response;
+
+		private MatchmakingThread(InetSocketAddress server) {
+			this.server = server;
+			try {
+				socket = new DatagramSocket();
+			} catch (SocketException e) {
 				e.printStackTrace();
+				throw new RuntimeException("Could not create matchmaking socket");
 			}
-		}).start();
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					wait();
+					byte[] bytes = message.getBytes();
+					socket.send(new DatagramPacket(bytes, bytes.length, server));
+					DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+					socket.receive(packet);
+					response = new String(packet.getData());
+				} catch (IOException | InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		private void send(String data) {
+			message = data;
+			notify();
+		}
+
+		private boolean isDone() {
+			return response != null;
+		}
+
+		private String get() {
+			String temp = response;
+			response = null;
+			return temp;
+		}
 	}
 
 	public interface MatchmakingListener {
