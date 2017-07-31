3805843ef7aa60be611b0386b7a081f02923d1a7
Ezard <ezard.ben@gmail.com>
2017-07-31 14:53:50 +0100

    Change animations paths to be absolute as opposed to relative to the animation subdirectory

--- core/src/main/java/com/ezardlabs/dethsquare/animation/Animations.java
+++ core/src/main/java/com/ezardlabs/dethsquare/animation/Animations.java
@@ -52,7 +52,7 @@ public class Animations {
 
 		private static AnimationData parse(String path) {
 			JSONObject json = new JSONObject(
-					String.join(" ", Dethsquare.IO.getFileLines("animations/" + path + "/anim.json")));
+					String.join(" ", Dethsquare.IO.getFileLines(path + "/anim.json")));
 
 			AnimationData data;
 			if (json.has("extends")) {
