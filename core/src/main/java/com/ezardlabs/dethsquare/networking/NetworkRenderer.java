b810e38b898f146605d36192265f96cef63b543b
Ezard <ezard.ben@gmail.com>
2017-10-21 13:02:33 +0100

    Synchronise tinting across the network

--- core/src/main/java/com/ezardlabs/dethsquare/networking/NetworkRenderer.java
+++ core/src/main/java/com/ezardlabs/dethsquare/networking/NetworkRenderer.java
@@ -16,6 +16,10 @@ public class NetworkRenderer extends NetworkScript {
 		data.putFloat(4, gameObject.renderer.height); // 4 - 7
 		data.putFloat(8, gameObject.renderer.xOffset); // 8 - 11
 		data.putFloat(12, gameObject.renderer.yOffset); // 12 - 15
+		data.putFloat(16, gameObject.renderer.getTint()[0]); // 16 - 19
+		data.putFloat(20, gameObject.renderer.getTint()[1]); // 20 - 23
+		data.putFloat(24, gameObject.renderer.getTint()[2]); // 14 - 27
+		data.putFloat(28, gameObject.renderer.getTint()[3]); // 28 - 31
 		return data;
 	}
 
@@ -23,10 +27,12 @@ public class NetworkRenderer extends NetworkScript {
 	public void onReceive(ByteBuffer data, int index) {
 		gameObject.renderer.setSize(data.getFloat(index), data.getFloat(index + 4));
 		gameObject.renderer.setOffsets(data.getFloat(index + 8), data.getFloat(index + 12));
+		gameObject.renderer.setTint(data.getFloat(index + 16), data.getFloat(index + 20), data.getFloat(index + 24),
+				data.getFloat(index + 28));
 	}
 
 	@Override
 	public short getSize() {
-		return 16;
+		return 32;
 	}
 }
